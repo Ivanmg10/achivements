6b77f4c65597bc8c3df97d8b8d13e25a70065052
Ivanin <ivanmg10@outlook.es>
2026-03-04 16:47:48 +0100

    feat: fix type

--- src/app/apitest/page.tsx
+++ src/app/apitest/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 
 export default function ApiTest() {
-  const [data, setData] = useState<any>(null);
+  const [data, setData] = useState<object | null>(null);
   useEffect(() => {
     fetch("/api/getUserProfile")
       .then((res) => res.json())
